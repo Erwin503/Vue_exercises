24e8035a654d7bab890a43318cd2ab4c0097668f
Ervin A <chelovek.ap@gmail.com>
2023-02-02 22:04:49 +0300

    Still learning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <p>Hello world!</p>
+  <div class="body">
+    <div class="main">
+      <p>{{ name }}</p>
+    </div>
+    <div class="main">
+      <p>{{ surn }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      name: "Ervin",
+      surn: "Smith",
+    };
+  },
+};
 </script>
