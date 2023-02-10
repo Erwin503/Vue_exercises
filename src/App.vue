1fdfa2a7137af3b66427567280c7dccb70dcfae5
Ervin A <chelovek.ap@gmail.com>
2023-02-10 12:50:56 +0300

    task21

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
   <div class="body">
     <div class="main">
       <p v-if="visible[0]">Ervin</p>
-      <button @click="toggle(0)">toggle</button>
+      <button @click="toggle(0)">{{ visible[0] ? "hide" : "show" }}</button>
     </div>
     <div class="main">
       <p v-if="visible[1]">Ervin</p>
-      <button @click="toggle(1)">toggle</button>
+      <button @click="toggle(1)">{{ visible[1] ? "hide" : "show" }}</button>
     </div>
     <div class="main">
       <p v-if="visible[2]">Ervin</p>
-      <button @click="toggle(2)">toggle</button>
+      <button @click="toggle(2)">{{ visible[2] ? "hide" : "show" }}</button>
     </div>
   </div>
 </template>
