bfb0572527c3f8adfb14a1aaad6a37a87a5bf538
Ervin A <chelovek.ap@gmail.com>
2023-02-10 12:54:16 +0300

    task22

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="body">
     <div class="main">
-      <p v-if="visible[0]">Ervin</p>
-      <button @click="toggle(0)">{{ visible[0] ? "hide" : "show" }}</button>
-    </div>
-    <div class="main">
-      <p v-if="visible[1]">Ervin</p>
-      <button @click="toggle(1)">{{ visible[1] ? "hide" : "show" }}</button>
-    </div>
-    <div class="main">
-      <p v-if="visible[2]">Ervin</p>
-      <button @click="toggle(2)">{{ visible[2] ? "hide" : "show" }}</button>
+      <p v-if="isAdmin">Ervin</p>
+      <p v-else>Not Ervin</p>
     </div>
   </div>
 </template>
@@ -19,13 +11,13 @@
 export default {
   data() {
     return {
-      visible: [true, true, true],
+      isAdmin: false,
     };
   },
-  methods: {
-    toggle: function (n) {
-      this.visible[n] = !this.visible[n];
-    },
-  },
+  // methods: {
+  //   toggle: function (n) {
+  //     this.visible[n] = !this.visible[n];
+  //   },
+  // },
 };
 </script>
