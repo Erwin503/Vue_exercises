146b0e07df48dd6e6557801d342262085feca64c
Ervin A <chelovek.ap@gmail.com>
2023-02-14 15:43:07 +0300

    task24

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="body">
     <div class="main">
-      <div v-if="visible">
+      <div v-show="visible">
         <p>ERVIN</p>
         <p>ERVIN</p>
         <p>ERVIN</p>
+        <!-- 2 задание
+        v-show отрисовывается всегда и меняется только css
+        v-if не отрисовывается если значение ложно
+        3 задание
+        v-show, если переключения будут частыми, и v-if, если условие может и не измениться во время исполнения 
+        4 задание
+        нельзя использовать v-else-->
       </div>
     </div>
     <dib class="main">
