d187e9ca1f4b1b14e98a3a745b41d729ddc234fe
Ervin A <chelovek.ap@gmail.com>
2023-02-14 16:45:02 +0300

    task36

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <template>
   <div class="body">
     <div class="main">
-      <ul>
-        <li v-for="i in arr">{{ i }}</li>
-      </ul>
+      <p>Ervin</p>
     </div>
-    <div class="main"><button @click="add">add</button></div>
-    <div class="main"><button @click="del">del</button></div>
-    <div class="main"><button @click="strdel">strdel</button></div>
-    <div class="main"><button @click="sort">sort</button></div>
-    <div class="main"><button @click="rev">rev</button></div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      arr: ["b", "a", "c"],
-    };
+    return {};
   },
-  methods: {
-    add: function () {
-      this.arr.push("xxx");
-    },
-    del: function () {
-      this.arr.pop();
-    },
-    strdel: function () {
-      this.arr.splice(this.arr.length - 2, 1);
-    },
-    sort: function () {
-      this.arr.sort();
-    },
-    rev: function () {
-      this.arr.reverse();
-    },
-  },
+  methods: {},
 };
 </script>
+<style>
+p {
+  color: red;
+}
+</style>
