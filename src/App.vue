fc7a98f9f21d288a7435e9cbdc0970c0048dcb32
Ervin A <chelovek.ap@gmail.com>
2023-02-14 16:19:07 +0300

    task32

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="body">
     <div class="main">
-      <ul>
-        <li v-for="i in hrefs">
-          <a href="{{ i.href }}">{{ i.text }}</a>
-        </li>
-      </ul>
-    </div>
-    <div class="main">
-      <tr v-for="p in products">
-        <td>{{ p.name }}</td>
-        <td>{{ p.price }}</td>
-        <td>{{ p.quantity }}</td>
-      </tr>
+      <p v-for="i in 30">{{ i }}</p>
     </div>
   </div>
 </template>
@@ -20,35 +9,8 @@
 <script>
 export default {
   data() {
-    return {
-      hrefs: [
-        { href: "1.html", text: "text1" },
-        { href: "2.html", text: "text2" },
-        { href: "3.html", text: "text3" },
-      ],
-      products: [
-        {
-          name: "product1",
-          price: 100,
-          quantity: 5,
-        },
-        {
-          name: "product2",
-          price: 200,
-          quantity: 4,
-        },
-        {
-          name: "product3",
-          price: 300,
-          quantity: 3,
-        },
-      ],
-    };
+    return {};
   },
-  methods: {
-    toggle: function (n) {
-      this.visible = !this.visible;
-    },
-  },
+  methods: {},
 };
 </script>
