33e6810841c94fc0bedff9ba259e9e233edae1c8
Ervin A <chelovek.ap@gmail.com>
2023-02-14 16:26:06 +0300

    task34

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="body">
     <div class="main">
-      <template v-for="i in items">
-        <p v-if="i > 0">{{ i }}</p>
-      </template>
+      <ul>
+        <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -11,7 +11,22 @@
 <script>
 export default {
   data() {
-    return { items: [1, -2, 3, -4, 5] };
+    return {
+      products: [
+        {
+          id: 1,
+          name: "product1",
+        },
+        {
+          id: 2,
+          name: "product2",
+        },
+        {
+          id: 3,
+          name: "product3",
+        },
+      ],
+    };
   },
   methods: {},
 };
