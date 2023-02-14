2767527f14735425415c9b791154c0fa25471eab
Ervin A <chelovek.ap@gmail.com>
2023-02-14 16:39:41 +0300

    task35

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,14 @@
   <div class="body">
     <div class="main">
       <ul>
-        <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+        <li v-for="i in arr">{{ i }}</li>
       </ul>
     </div>
+    <div class="main"><button @click="add">add</button></div>
+    <div class="main"><button @click="del">del</button></div>
+    <div class="main"><button @click="strdel">strdel</button></div>
+    <div class="main"><button @click="sort">sort</button></div>
+    <div class="main"><button @click="rev">rev</button></div>
   </div>
 </template>
 
@@ -12,22 +17,25 @@
 export default {
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: "product1",
-        },
-        {
-          id: 2,
-          name: "product2",
-        },
-        {
-          id: 3,
-          name: "product3",
-        },
-      ],
+      arr: ["b", "a", "c"],
     };
   },
-  methods: {},
+  methods: {
+    add: function () {
+      this.arr.push("xxx");
+    },
+    del: function () {
+      this.arr.pop();
+    },
+    strdel: function () {
+      this.arr.splice(this.arr.length - 2, 1);
+    },
+    sort: function () {
+      this.arr.sort();
+    },
+    rev: function () {
+      this.arr.reverse();
+    },
+  },
 };
 </script>
