b6161670a8ef7712d35f956e334f2bb896880088
Ervin A <chelovek.ap@gmail.com>
2023-06-17 16:52:22 +0300

    task47

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,26 @@
   <div class="body">
     <div class="main">
       <p>Task 1</p>
-      <input type="checkbox" v-model="checked" />
-      <p v-if="checked">Test</p>
+      <input type="checkbox" v-model="arr" value="Python" /> <span>Python</span
+      ><br />
+      <input type="checkbox" v-model="arr" value="Java" /> <span>Java</span
+      ><br />
+      <input type="checkbox" v-model="arr" value="JS" /> <span>JS</span><br />
+    </div>
+    <div class="main">
+      <input type="checkbox" v-model="arr" value="C++" /> <span>C++</span><br />
+      <input type="checkbox" v-model="arr" value="C" /> <span>C</span><br />
+      <input type="checkbox" v-model="arr" value="GO" /> <span>GO</span><br />
+      <input type="checkbox" v-model="arr" value="Ruby" /> <span>Ruby</span
+      ><br />
+    </div>
+
+    <div class="main">
+      <p>Количество: {{ arr.length }}</p>
+      <p>Cписок языков:</p>
+      <ul v-for="elem in arr">
+        <li>{{ elem }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -12,14 +30,10 @@
 export default {
   data() {
     return {
-      checked: false,
+      arr: [],
     };
   },
 
-  methods: {
-    press: function () {
-      this.checked = !this.checked;
-    },
-  },
+  methods: {},
 };
 </script>
