9250c3020fa9b1e7e4134412eb22a59b80e6b42d
Ervin A <chelovek.ap@gmail.com>
2023-06-17 16:28:06 +0300

    task41

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="body">
     <div class="main">
-      <div id="cmd">
-        <p :class="obj">1</p>
-        <p class="done">2</p>
-      </div>
-    </div>
-    <div class="main">
-      <button @click="hide">Hide</button>
-    </div>
-    <div class="main">
-      <button @click="show">Show</button>
-    </div>
-    <div class="main">
-      <button @click="toggle">Toggle</button>
+      <p :class="{ active: isDisabled, valid: isValid }">Test</p>
     </div>
   </div>
 </template>
@@ -22,23 +10,11 @@
 export default {
   data() {
     return {
-      obj: {
-        hidden: true,
-        done: true,
-      },
+      isValid: true,
+      isDisabled: true,
     };
   },
 
-  methods: {
-    hide: function () {
-      this.obj.hidden = true;
-    },
-    show: function () {
-      this.obj.hidden = false;
-    },
-    toggle: function () {
-      this.obj.hidden = !this.obj.hidden;
-    },
-  },
+  methods: {},
 };
 </script>
