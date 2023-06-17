a9cab9dd0017c4f3b1bf5a83fdec11b9c410c7a7
Ervin A <chelovek.ap@gmail.com>
2023-06-17 18:47:30 +0300

    task56

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,22 @@
         <th>age</th>
       </tr>
       <tr v-for="user in users" :key="user.id">
-        <th>{{ user.id }}</th>
-        <th>{{ user.name }}</th>
-        <th>{{ user.salary }}</th>
-        <th>{{ user.age }}</th>
-        <th @click="removeItem(user.id)">del</th>
+        <template v-if="!user.isEdit">
+          <th>{{ user.id }}</th>
+          <th>{{ user.name }}</th>
+          <th>{{ user.salary }}</th>
+          <th>{{ user.age }}</th>
+          <th @click="removeItem(user.id)">del</th>
+          <th @click="editItem(user)">edit</th>
+        </template>
+
+        <template v-else>
+          <p>> Edit forms</p>
+          <span>Name: </span><input v-model="user.name" /><br />
+          <span>Salary: </span><input v-model="user.salary" /><br />
+          <span>Age: </span><input v-model="user.age" /><br />
+          <button @click="save(user)" id="save_but">save</button>
+        </template>
       </tr>
     </table>
   </div>
@@ -31,18 +42,21 @@ export default {
           name: "name1",
           salary: 100,
           age: 30,
+          isEdit: false,
         },
         {
           id: 2,
           name: "name2",
           salary: 200,
           age: 40,
+          isEdit: false,
         },
         {
           id: 3,
           name: "name3",
           salary: 300,
           age: 50,
+          isEdit: false,
         },
       ],
     };
@@ -54,27 +68,12 @@ export default {
         return user.id !== id;
       });
     },
+    editItem: function (user) {
+      user.isEdit = true;
+    },
+    save: function (user) {
+      user.isEdit = false;
+    },
   },
 };
 </script>
-
-<style>
-#cmd p,
-li,
-span {
-  font-family: Consolas;
-  font-size: 1.2em;
-  color: rgba(0, 199, 76);
-}
-textarea {
-  color: black;
-}
-table {
-  border-collapse: collapse;
-}
-th {
-  border: 1px solid rgba(0, 199, 76);
-  color: rgba(0, 199, 76);
-  width: 60px;
-}
-</style>
